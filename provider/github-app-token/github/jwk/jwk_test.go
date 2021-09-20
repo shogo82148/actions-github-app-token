f08d07641cdcca55c85f27ee592f758925c483d5
Ichinose Shogo <shogo82148@gmail.com>
2021-09-20 19:58:33 +0900

    add jwks

--- provider/github-app-token/github/jwk/jwk_test.go
+++ provider/github-app-token/github/jwk/jwk_test.go
@@ -5,9 +5,18 @@ import (
 	"testing"
 )
 
-func TestAppendixA(t *testing.T) {
+func TestKeyAppendixA(t *testing.T) {
 	t.Run("RFC 7517 A.1. Example Public Keys (RSA)", func(t *testing.T) {
-		rawKey := `{"kty":"RSA","n":"0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw","e":"AQAB","alg":"RS256","kid":"2011-04-29"}`
+		rawKey := `{"kty":"RSA",` +
+			`"n":"0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx` +
+			`4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMs` +
+			`tn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2` +
+			`QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbI` +
+			`SD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqb` +
+			`w0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",` +
+			`"e":"AQAB",` +
+			`"alg":"RS256",` +
+			`"kid":"2011-04-29"}`
 		key, err := ParseKey([]byte(rawKey))
 		if err != nil {
 			t.Fatal(err)
