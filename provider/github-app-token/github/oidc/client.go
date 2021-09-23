33506b3bcd95268f928870cd08b573d5fd5a3e47
Ichinose Shogo <shogo82148@gmail.com>
2021-09-23 12:36:31 +0900

    verify the certificates

--- provider/github-app-token/github/oidc/client.go
+++ provider/github-app-token/github/oidc/client.go
@@ -2,10 +2,14 @@ package oidc
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/rsa"
 	"encoding/hex"
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -51,6 +55,8 @@ func NewClient(httpClient Doer, issuer string, thumbprints []string) (*Client, e
 
 func (c *Client) ParseWithClaims(ctx context.Context, tokenString string, claims jwt.Claims) (*jwt.Token, error) {
 	return jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		now := time.Now()
+
 		// get JSON Web Key Set
 		config, err := c.GetConfig(ctx)
 		if err != nil {
@@ -71,17 +77,28 @@ func (c *Client) ParseWithClaims(ctx context.Context, tokenString string, claims
 			return nil, errors.New("oidc: key is not found")
 		}
 
+		// verify the certificates
+		for _, cert := range key.X509CertificateChain() {
+			if now.After(cert.NotAfter) {
+				return nil, errors.New("oidc: the certificate is expired")
+			}
+			if now.Before(cert.NotBefore) {
+				return nil, errors.New("oidc: the certificate is not valid yet")
+			}
+		}
+
 		// verify signing method
-		switch key.KeyType() {
-		case "RSA":
+		publicKey := key.PublicKey()
+		switch publicKey.(type) {
+		case *rsa.PublicKey:
 			if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 				return nil, errors.New("oidc: unexpected signing method")
 			}
-		case "EC":
+		case *ecdsa.PublicKey:
 			if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
 				return nil, errors.New("oidc: unexpected signing method")
 			}
-		case "OKP":
+		case ed25519.PublicKey:
 			if _, ok := token.Method.(*jwt.SigningMethodEd25519); !ok {
 				return nil, errors.New("oidc: unexpected signing method")
 			}
