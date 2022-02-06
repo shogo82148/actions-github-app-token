a2fc3f62354e958cf11867c0b7282a7c6d7ebb56
Ichinose Shogo <shogo82148@gmail.com>
2022-02-06 13:29:26 +0900

    suppress dups

--- provider/github-app-token/github/memoize/memoize.go
+++ provider/github-app-token/github/memoize/memoize.go
@@ -22,6 +22,19 @@ type Memo[K comparable, V any] struct {
 type entry[V any] struct {
 	val       V
 	expiresAt time.Time
+	call      *call[V]
+}
+
+type call[V any] struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	runs   int
+	chans  []chan<- result[V]
+}
+
+type result[V any] struct {
+	val V
+	err error
 }
 
 // Call calls memoized Func.
@@ -41,16 +54,58 @@ func (memo *Memo[K, V]) Call(ctx context.Context, key K) (V, error) {
 		}
 	} else {
 		e = &entry[V]{}
+		memo.m[key] = e
 	}
-	v, expiresAt, err := memo.Func(ctx, key)
-	if err != nil {
-		memo.mu.Unlock()
-		return v, err
+
+	// the cache is expired or unavailable.
+	// call memo.Func
+	c := e.call
+	if c == nil {
+		c = new(call[V])
+		c.ctx, c.cancel = context.WithCancel(context.Background())
+		e.call = c
+		go doCall(memo, e, c, key)
 	}
-	e.val = v
-	e.expiresAt = expiresAt
-	memo.m[key] = e
+	ch := make(chan result[V], 1)
+	c.chans = append(c.chans, ch)
+	c.runs++
 	memo.mu.Unlock()
 
-	return v, nil
+	select {
+	case ret := <-ch:
+		return ret.val, ret.err
+	case <-ctx.Done():
+		memo.mu.Lock()
+		c.runs--
+		if c.runs == 0 {
+			c.cancel()
+		}
+		memo.mu.Unlock()
+		var zero V
+		return zero, ctx.Err()
+	}
+}
+
+func doCall[K comparable, V any](memo *Memo[K, V], e *entry[V], c *call[V], key K) {
+	// call memo.Func
+	v, expiresAt, err := memo.Func(c.ctx, key)
+	ret := result[V]{
+		val: v,
+		err: err,
+	}
+
+	// save the cache
+	memo.mu.Lock()
+	e.call = nil
+	chans := c.chans
+	if err == nil {
+		e.val = v
+		e.expiresAt = expiresAt
+	}
+	memo.mu.Unlock()
+
+	// notify the result to the callers
+	for _, ch := range chans {
+		ch <- ret
+	}
 }
--- provider/github-app-token/github/memoize/memoize_test.go
+++ provider/github-app-token/github/memoize/memoize_test.go
@@ -2,6 +2,8 @@ package memoize
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -48,5 +50,52 @@ func TestCall(t *testing.T) {
 	}
 	if got != 2 {
 		t.Errorf("want 2, got %d", got)
+	}
+}
+
+func TestDoDupSuppress(t *testing.T) {
+	var wg1, wg2 sync.WaitGroup
+	c := make(chan string, 1)
+	var calls int32
+	g := Memo[string, string]{
+		Func: func(ctx context.Context, key string) (string, time.Time, error) {
+			if atomic.AddInt32(&calls, 1) == 1 {
+				// First invocation.
+				wg1.Done()
+			}
+			v := <-c
+			c <- v // pump; make available for any future calls
+
+			time.Sleep(10 * time.Millisecond) // let more goroutines enter Do
+
+			return v, time.Time{}, nil
+		},
+	}
+
+	const n = 10
+	wg1.Add(1)
+	for i := 0; i < n; i++ {
+		wg1.Add(1)
+		wg2.Add(1)
+		go func() {
+			defer wg2.Done()
+			wg1.Done()
+			v, err := g.Call(context.Background(), "key")
+			if err != nil {
+				t.Errorf("Do error: %v", err)
+				return
+			}
+			if v != "bar" {
+				t.Errorf("Do = %T %v; want %q", v, v, "bar")
+			}
+		}()
+	}
+	wg1.Wait()
+	// At least one goroutine is in fn now and all of them have at
+	// least reached the line before the Do.
+	c <- "bar"
+	wg2.Wait()
+	if got := atomic.LoadInt32(&calls); got <= 0 || got >= n {
+		t.Errorf("number of calls = %d; want over 0 and less than %d", got, n)
 	}
 }
