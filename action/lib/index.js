009b698c90ff4e6f7715def81f763b23a0d275d8
Ichinose Shogo <shogo82148@gmail.com>
2022-10-13 11:48:21 +0900

    bump up to v0.0.5

--- action/lib/index.js
+++ action/lib/index.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -76,7 +80,7 @@ async function assumeRole(params) {
     const result = await client.postJson(params.providerEndpoint, payload, headers);
     if (result.statusCode !== http.HttpCodes.OK) {
         const resp = result.result;
-        core.setFailed((resp === null || resp === void 0 ? void 0 : resp.message) || "unknown error");
+        core.setFailed(resp?.message || "unknown error");
         return;
     }
     const resp = result.result;
