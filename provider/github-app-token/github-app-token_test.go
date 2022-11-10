5011c5249461c46b62762ea8d9143df9033cc0ce
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-10 20:16:39 +0900

    add test

--- provider/github-app-token/github-app-token_test.go
+++ provider/github-app-token/github-app-token_test.go
@@ -2,8 +2,10 @@ package githubapptoken
 
 import (
 	"context"
+	"testing"
 
 	"github.com/shogo82148/actions-github-app-token/provider/github-app-token/github"
+	"github.com/shogo82148/goat/jwt"
 )
 
 type githubClientMock struct {
@@ -34,5 +36,36 @@ func (c *githubClientMock) ParseIDToken(ctx context.Context, idToken string) (*g
 	return c.ParseIDTokenFunc(ctx, idToken)
 }
 
-// TODO: write tests
-var _ githubClientMock
+func TestHandle(t *testing.T) {
+	h := &Handler{
+		github: &githubClientMock{
+			ValidateAPIURLFunc: func(url string) error {
+				return nil
+			},
+			ParseIDTokenFunc: func(ctx context.Context, idToken string) (*github.ActionsIDToken, error) {
+				return &github.ActionsIDToken{
+					Claims: &jwt.Claims{
+						Audience: []string{"https://github-app.shogo82148.com/1234567890"},
+					},
+					Repository: "shogo82148/actions-github-app-token",
+				}, nil
+			},
+			GetReposInstallationFunc: func(ctx context.Context, owner, repo string) (*github.GetReposInstallationResponse, error) {
+				return &github.GetReposInstallationResponse{}, nil
+			},
+			CreateAppAccessTokenFunc: func(ctx context.Context, installationID uint64, permissions *github.CreateAppAccessTokenRequest) (*github.CreateAppAccessTokenResponse, error) {
+				return &github.CreateAppAccessTokenResponse{
+					Token: "github-app-token",
+				}, nil
+			},
+		},
+		appID: 1234567890,
+	}
+	resp, err := h.handle(context.Background(), "dummy-token", &requestBody{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.GitHubToken != "github-app-token" {
+		t.Errorf("unexpected token")
+	}
+}
