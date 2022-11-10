0e8feea978d8278c96852494ac0d7859afc77cea
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-10 17:59:23 +0900

    add test for custom audience

--- provider/github-app-token/github/parse_id_token_test.go
+++ provider/github-app-token/github/parse_id_token_test.go
@@ -23,50 +23,90 @@ func TestParseIDToken_Integrated(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	t.Logf("the request started at %s", time.Now())
-	token, err := getIdToken(ctx, idToken, idURL)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("the id is issued at %s", time.Now())
+	t.Run("the default audience", func(t *testing.T) {
+		t.Logf("the request started at %s", time.Now())
+		token, err := getIdToken(ctx, "", idToken, idURL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("the id is issued at %s", time.Now())
 
-	oidcClient, err := oidc.NewClient(&oidc.ClientConfig{
-		Doer:   http.DefaultClient,
-		Issuer: oidcIssuer,
+		oidcClient, err := oidc.NewClient(&oidc.ClientConfig{
+			Doer:   http.DefaultClient,
+			Issuer: oidcIssuer,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		c := &Client{
+			baseURL:    apiBaseURL,
+			httpClient: http.DefaultClient,
+			oidcClient: oidcClient,
+		}
+		id, err := c.ParseIDToken(ctx, token)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("raw: %#v", id.Raw)
+
+		if got, want := id.Actor, os.Getenv("GITHUB_ACTOR"); got != want {
+			t.Errorf("unexpected actor: want %q, got %q", want, got)
+		}
+		if got, want := id.Repository, os.Getenv("GITHUB_REPOSITORY"); got != want {
+			t.Errorf("unexpected repository: want %q, got %q", want, got)
+		}
+		if got, want := id.EventName, os.Getenv("GITHUB_EVENT_NAME"); got != want {
+			t.Errorf("unexpected repository: want %q, got %q", want, got)
+		}
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	c := &Client{
-		baseURL:    apiBaseURL,
-		httpClient: http.DefaultClient,
-		oidcClient: oidcClient,
-	}
-	id, err := c.ParseIDToken(ctx, token)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("raw: %#v", id.Raw)
+	t.Run("custom audience", func(t *testing.T) {
+		t.Logf("the request started at %s", time.Now())
+		token, err := getIdToken(ctx, "https://example.com", idToken, idURL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("the id is issued at %s", time.Now())
 
-	if got, want := id.Actor, os.Getenv("GITHUB_ACTOR"); got != want {
-		t.Errorf("unexpected actor: want %q, got %q", want, got)
-	}
-	if got, want := id.Repository, os.Getenv("GITHUB_REPOSITORY"); got != want {
-		t.Errorf("unexpected repository: want %q, got %q", want, got)
-	}
-	if got, want := id.EventName, os.Getenv("GITHUB_EVENT_NAME"); got != want {
-		t.Errorf("unexpected repository: want %q, got %q", want, got)
-	}
+		oidcClient, err := oidc.NewClient(&oidc.ClientConfig{
+			Doer:   http.DefaultClient,
+			Issuer: oidcIssuer,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		c := &Client{
+			baseURL:    apiBaseURL,
+			httpClient: http.DefaultClient,
+			oidcClient: oidcClient,
+		}
+		id, err := c.ParseIDToken(ctx, token)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("raw: %#v", id.Raw)
+
+		if got, want := id.Actor, os.Getenv("GITHUB_ACTOR"); got != want {
+			t.Errorf("unexpected actor: want %q, got %q", want, got)
+		}
+		if got, want := id.Repository, os.Getenv("GITHUB_REPOSITORY"); got != want {
+			t.Errorf("unexpected repository: want %q, got %q", want, got)
+		}
+		if got, want := id.EventName, os.Getenv("GITHUB_EVENT_NAME"); got != want {
+			t.Errorf("unexpected repository: want %q, got %q", want, got)
+		}
+	})
 }
 
-func getIdToken(ctx context.Context, idToken, idURL string) (string, error) {
+func getIdToken(ctx context.Context, audience, idToken, idURL string) (string, error) {
 	u, err := url.Parse(idURL)
 	if err != nil {
 		return "", err
 	}
 	q := u.Query()
-	q.Set("audience", "https://github.com/shogo82148/another-app")
+	q.Set("audience", audience)
 	u.RawQuery = q.Encode()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
