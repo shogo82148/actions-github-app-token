efcffe31cfc45f3f1800c5636bab90f625c0d089
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-08 23:10:16 +0900

    migrate ctxlog to slog

--- provider/github-app-token/cmd/bootstrap/main.go
+++ provider/github-app-token/cmd/bootstrap/main.go
@@ -16,7 +16,9 @@ var logger *slog.Logger
 
 func init() {
 	// initialize the logger
-	h1 := slog.NewJSONHandler(os.Stderr, nil)
+	h1 := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
 	h2 := xrayslog.NewHandler(h1, "trace_id")
 	logger = slog.New(h2)
 	slog.SetDefault(logger)
@@ -32,7 +34,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", h)
 
-	logger := httplogger.NewSlogLogger(slog.LevelInfo, "message for http access", logger)
+	logger := httplogger.NewSlogLogger(slog.LevelInfo, "http access log", logger)
 
 	err = ridgenative.ListenAndServe(":8080", httplogger.LoggingHandler(logger, mux))
 	if err != nil {
--- provider/github-app-token/go.mod
+++ provider/github-app-token/go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssm v1.37.5
 	github.com/goccy/go-yaml v1.11.0
 	github.com/shogo82148/aws-xray-yasdk-go v1.7.0
-	github.com/shogo82148/ctxlog v0.1.0
 	github.com/shogo82148/go-http-logger v1.3.0
 	github.com/shogo82148/goat v0.0.6
 	github.com/shogo82148/ridgenative v1.4.0
