22144ae063b305b3230b34696a87b13f870d4a4e
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-08 22:53:51 +0900

    initialize slog

--- provider/github-app-token/cmd/bootstrap/main.go
+++ provider/github-app-token/cmd/bootstrap/main.go
@@ -6,9 +6,19 @@ import (
 	"os"
 
 	githubapptoken "github.com/shogo82148/actions-github-app-token/provider/github-app-token"
+	"github.com/shogo82148/aws-xray-yasdk-go/xray/xraylog"
+	"github.com/shogo82148/aws-xray-yasdk-go/xray/xrayslog"
 	httplogger "github.com/shogo82148/go-http-logger"
 	"github.com/shogo82148/ridgenative"
 )
+
+func init() {
+	// initialize the logger
+	h1 := slog.NewJSONHandler(os.Stderr, nil)
+	h2 := xrayslog.NewHandler(h1, "trace_id")
+	slog.SetDefault(slog.New(h2))
+	xraylog.SetLogger(xrayslog.NewXRayLogger(h2))
+}
 
 func main() {
 	h, err := githubapptoken.NewHandler()
